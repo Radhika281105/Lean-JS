d039602b53e2603b5e7dbb95647d994c0b7e3c1f
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-20 01:34:27 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "radhika@gmail.com"
+var accountPassword = 212121
+let accountCity = "Jaipur"
+
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// prefer not to use "var" becoz of issue in block scope and functional scope
+
+// if we declare a variable and don't define it then in output  we get "undefined"
