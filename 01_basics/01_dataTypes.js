be34e727ef18d5353168c0fe573f561338f99e8c
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-20 01:53:18 +0000

    container config done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 3 + 3)  //in vs code we'll get error as we are using node.js , not browser
+
+// code readability is priority
+console.log("Hitesh"); console.log("Radhika")
+
+let name = "Radhika"  //string
+let age = 20 //number
+let isLoggedin = true //boolean
+let state
+
+//Primitive Datatypes
+//number => 2 to the pwr 53
+//string
+//bigint - for larger values
+//boolean - true/false
+//null => standalone value
+// undefined 
+//symbol => uniquness/unique
+
+//object
+
+console.log(typeof "radhika")
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
