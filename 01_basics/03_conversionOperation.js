1f6d15b17461400eca9d2dd915646077c602d1a1
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-21 13:43:58 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,28 @@
+// investigation study
+
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(valueInNumber)
-console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// "33" => 33
+//  "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = ""
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+// 1 -> true ; 0 -> false
+// "" -> false
+// "radhika" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
