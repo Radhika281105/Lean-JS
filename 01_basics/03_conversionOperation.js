275611a32eb408c13631c4838b1d5abc67957023
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-21 14:04:06 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,43 @@ let isLoggedIn = ""
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ****************Operations************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " radhika"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log(1 + "2" + 2) //12
+// console.log("1" + "2") // 12
+
+
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(+true)
+// console.log(true+) //error
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// tricky behaviour like this don't look good
+
+
