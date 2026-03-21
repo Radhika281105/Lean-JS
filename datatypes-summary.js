b1c3e0a50e1c1759777a1ce3b694de0c2095ff4e
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-21 14:59:57 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive Type
+// call-by value -> value is copied but not actual memory addr is given
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+// No special category given to float..It is given under Number
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; //undefined
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+// given same value but still they are not same due to symbol data type
+// console.log(id == anotherId)
+
+// to convert large number into bigInt
+const bigNumber = 37774986864234n
+// console.log(typeof bigNumber)
+
+
+// Reference Type (Non-Primtive)
+// memory reference is allocated directly
+
+// Array, Objects, Functions
+
+// arrays
+const heros = ["shaktiman", "naagraj", "doga"]
+
+// objects in curly braces
+let myObj = {
+    name: "radhika",
+    age: 22,
+}
+
+// function
+const myFunction = function(){
+    console.log("Hello world");
+}
+
