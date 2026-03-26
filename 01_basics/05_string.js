383f0f028d6ee599b017281ce081aa94b6c9fe19
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-26 13:48:23 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,11 +6,11 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 // new is used to assign an object to a variable
-const getName = new String("radhika")
+const getName = new String("radhika-ct")
 console.log(getName[0]);
 
 // to access the object
-console.log(getName.__proto__);
+console.log(getName.__proto__); //{}
 
 // access the length
 console.log(getName.length);
@@ -19,7 +19,36 @@ console.log(getName.length);
 console.log(getName.toUpperCase());
 
 // to find the character present at each index
-console.log(getName.charAt(9))
+console.log(getName.charAt(5))
 
 // to find index of the characters
 console.log(getName.indexOf('a')) //if not found then index -> -1
+
+// exclude last index char i.e. 4
+const newString = getName.substring(0,4);
+console.log(newString)
+
+// can give negative index
+// her also lower bound index value neglecgted i.e. -2 index
+const anotherString = getName.slice(-4,-2);
+console.log(anotherString);
+
+const newStringOne = "  radhika   "
+console.log(newStringOne);
+// trim removes starting and ending extra spaces
+console.log(newStringOne.trim());
+
+// after radhika if u put some dash then it might be converted into %20
+const url = "https://radhika.com/radhika%20saxena"
+// replace helps in changing or replacing the value
+console.log(url.replace('%20','-'))
+
+// return true or false based on if the given string is present or not
+console.log(url.includes('radhika')); //true
+console.log(url.includes('my')); //false
+
+// to split the string using a specified string 
+// split() method
+const new_url = url.split('/');
+// [ 'https:', '', 'radhika.com', 'radhika%20saxena' ]
+console.log(new_url);
