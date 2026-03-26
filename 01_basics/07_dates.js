552cc95abcca3d6042fa46267e078276ed7b4e71
RADHIKA SAXENA <157279703+Radhika2811055@users.noreply.github.com>
2026-03-26 14:45:42 +0000

    dates and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+// trying to make date as global
+
+// DATES
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate); //object
+
+// month start from 0 index
+let myCreateDate = new Date(2023,0,23);
+let myCreateDate1 = new Date("2023-01-23");
+console.log(myCreateDate.toDateString())
+console.log(myCreateDate1.toDateString())
